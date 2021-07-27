261b7bec838d9cbd249508f5569a4a161f5c364a
Michael Weber <87260548+gxai@users.noreply.github.com>
2021-07-27 20:47:09 +0300

    Create favicon.js

---
+++ favicon.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+  var link = top.document.createElement("link");
+  link.type = "image/x-icon";
+  link.rel = "shortcut icon";
+  link.href = "/Michael-Weber.ico";
+  top.document.getElementsByTagName("head")[0].appendChild(link);
+}
